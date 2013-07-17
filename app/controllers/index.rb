ef3beef5cb70d431bf88a020ad0ace481744aa58
Apprentice <apprentice@devbootcamp.com>
2013-07-17 16:30:57 -0700

    done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,48 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+post '/create_user' do
+  user = User.new(email: params[:email], 
+                  first_name: params[:first_name], 
+                  last_name: params[:last_name] )
+  user.password = params[:password]
+
+  if user.save
+    @user_success = true 
+  else
+    @user_success = false
+  end
+
+  erb :index
+  # @validated_account = user.valid?
+
+  # if user.valid?
+  #   @bad_account = false
+
+  # else
+  #   @bad_account = true
+  # end
+end
+
+post '/login' do
+  user = User.authenticate(params[:email], params[:password])
+
+  if user
+    session[:user] = user
+    @user_login_success = true
+  else
+    session[:user] = nil
+    @user_login_success = false
+  end
+
+  erb :index
+end
+
+get '/secretpage' do
+  if session[:user]
+    erb :secretpage
+  else
+    redirect to('/')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,34 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email,password)
+    user = self.where(email: email)[0]
+
+    if user
+      if user.password == password
+        puts user.password
+        return user
+      end
+    end
+
+    return nil
+
+  end
+
 end
